18e8af18c7ce9fce5da04cb7bbcf93ce712ac597
Nik <i.p.chn.k@gmail.com>
2026-01-17 02:45:07 +0300

    Исправлена маршрутизация для отображения глвной страницы

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,22 +1,22 @@
 function Contacts() {
-  return (
-    <section className="section section--compact">
-      <div className="section__head">
-        <h2>Контакты</h2>
-        <p>Связаться с командой и предложить тему.</p>
-      </div>
-      <div className="contact-card">
-        <div>
-          <strong>Email</strong>
-          <span>team@cyberbase.local</span>
-        </div>
-        <div>
-          <strong>Соцсети</strong>
-          <span>Telegram • VK • LinkedIn</span>
-        </div>
-      </div>
-    </section>
-  )
+    return (
+        <section className="section section--compact">
+            <div className="section__head">
+                <h2>Контакты</h2>
+                <p>Связаться с командой и предложить тему.</p>
+            </div>
+            <div className="contact-card">
+                <div>
+                    <strong>Email</strong>
+                    <span>team@cyberbase.local</span>
+                </div>
+                <div>
+                    <strong>Соцсети</strong>
+                    <span>Telegram • VK • LinkedIn</span>
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default Contacts
+export default Contacts;
